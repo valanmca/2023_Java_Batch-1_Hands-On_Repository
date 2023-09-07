182110f5e5e0019fba14441d42cd324d7350cabf
PravinKumarRajaSahaym <141833504+PravinKumarRajaSahaym@users.noreply.github.com>
2023-09-07 14:46:08 +0530

    Day-2

--- PravinKumarRajaSahayam-11946/FoundationalTrainingProjects/DataStructureProjects/Array_Exs_1.java
+++ PravinKumarRajaSahayam-11946/FoundationalTrainingProjects/DataStructureProjects/Array_Exs_1.java
@@ -1,23 +1,17 @@
-//Simple program in java
-package com.pravinkumar.day1;
-
+//Sum of Arrays using Type 3
+package com.pravinkumar.day2;
+import java.util.Scanner;
 public class Array_Exs_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-			int sId=101;
-			String sName= "Array";
-			int m1 = 50,m2=60,m3=70;
-			int sum = m1+m2+m3;
-			int avg = sum / 3;
-			System.out.println("Student Id:" + sId);
-			System.out.println("Student Name:" + sName);
-			System.out.println("Student Mark1:" + m1);
-			System.out.println("Student Mark2:" + m2);
-			System.out.println("Student Mark3:" + m3);
-			System.out.println("Total Marks:" + sum);
-			System.out.println("Average Marks:" + avg);
-		}
-
+    int arr[]=new int[] {10,20,30,40,50};
+    int sum=0;
+    for(int i=0;i<arr.length;i++)
+    {
+    	sum +=arr[i];
+    }
+    System.out.println("The sum is : " + sum);
 	}
+
+}
--- PravinKumarRajaSahayam-11946/FoundationalTrainingProjects/DataStructureProjects/Array_Exs_7.java
+++ PravinKumarRajaSahayam-11946/FoundationalTrainingProjects/DataStructureProjects/Array_Exs_7.java
@@ -1,35 +1,33 @@
-package com.pravinkumar.day1;
-import java.util.Scanner;
+package com.pravinkumar.day2;
+
+import java.util.ArrayList;
+
 public class Array_Exs_7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]=new int [10];
-      Scanner sc=new Scanner(System.in);
-      System.out.println("Enter the size of the Numbers:");
-      int array=sc.nextInt();
-      System.out.println("The Array are:");
-      for(int i=0;i<arr.length;i++)
-      {
-    	  arr[i]=sc.nextInt(array);
-      }
-      int max=1;
-      for(int i=0;i<arr.length;i++)
-      {
-      if(max<arr[i]) 
-      {
-    	  System.out.println("The Maximum number is:" + max);
-      }
-      }
-      int min=arr[0];
-      for(int i=1;i<arr.length;i++)
-      {
-    	  if(min>arr[i])
-    	  {
-    		  System.out.println("The Minimum number is:" + min);
-    	  }
-      }
-      
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// Summing the Number with the help of Index
+		// 150
+		int sum = 0;
+		int arr[] = { 10, 20, 30, 40, 50 };
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+			list.add(arr[i]);
+		}
 
-}
+		list.add(0, sum);
+
+		System.out.println("The List is:" + list);
+//[150,10,20,30,40,50,]
+		list.set(list.size() - 1, 100);
+		System.out.println(list);
+
+//[150,10,20,30,40,50,100]
+		list.remove(0);
+		System.out.println(list);
+//[10,20,30,40,100]
+
+	}
+
 }

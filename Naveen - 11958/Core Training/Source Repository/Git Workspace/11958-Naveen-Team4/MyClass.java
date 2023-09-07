474208709ae50f37f539b58b3fb0f8caea99e3e4
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2023-09-07 15:05:21 +0530

    Add files via upload

---
+++ Naveen - 11958/Core Training/Source Repository/Git Workspace/11958-Naveen-Team4/MyClass.java
@@ -0,0 +1,82 @@
+class MyClass {
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 84d02e26a59d7631fb125294a3d99fe2c2c55251
+// naveen - 11958
+void naveen1(){
+   System.out.println("Hello");
+}
+void naveen2(){
+   System.out.println("World");
+}
+<<<<<<< HEAD
+=======
+
+public void static main(String args[]){
+>>>>>>> 84d02e26a59d7631fb125294a3d99fe2c2c55251
+
+public void static main(String args[]){
+=======
+    void age() {
+        System.out.println("Age is 23");
+>>>>>>> 2fadf88fc9d9fb4d6b425f74ba585f857f12926a
+
+<<<<<<< HEAD
+    }
+
+    void name() {
+        System.out.println("Name: Jeyandhan");
+    }
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.age();
+        obj.name();
+    }
+
+// Age and name added by jeyandhan
+=======
+public static void Add(int a,int b) {
+   Sum=a+b;
+System.out.println("The value of sum is "+Sum);
+}
+
+public static void Subtract(int a,int b) {
+   Sub=a-b;
+System.out.println("The value of subtract is "+Sub);
+}
+
+// Add & Sub added by Purushoth
+
+>>>>>>> 1b2a59b681d155bf3c007c0164106ef8ff6a1f66
+}
+<<<<<<< HEAD
+<<<<<<< HEAD
+}
+=======
+
+=========
+/*public static void Multiplication(int a,int b) {
+   Mul=a*b;
+System.out.println("The value of sum is "+Multiplication);
+}
+
+public static void Division(int a,int b) {
+   Div=a/b;
+System.out.println("The value of subtract is "+Division);
+}*/
+
+// Multiplication and division added by Jeyandhan
+
+
+void name(){
+    System.out.println("This is Naveen-11958, Commented Nagajeyandhan Code and inserted my code")
+}
+
+
+
+>>>>>>> 2fadf88fc9d9fb4d6b425f74ba585f857f12926a
+=======
+}
+>>>>>>> 84d02e26a59d7631fb125294a3d99fe2c2c55251
---
+++ Naveen - 11958/Core Training/Source Repository/Git Workspace/Naveen-11958/MyClass.java
@@ -0,0 +1,28 @@
+class MyClass {
+
+/*public static void Add(int a,int b) {
+   Sum=a+b;
+System.out.println("The value of sum is "+Sum);
+}
+
+public static void Subtract(int a,int b) {
+   Sub=a-b;
+System.out.println("The value of subtract is "+Sub);
+}*/
+// Add & Sub added by Purushoth
+
+void naveen3(){
+   System.out.println("Commented the purushothaman code and inserted my code");
+}
+
+void naveen1(){
+   System.out.println("Bit");
+}
+
+void naveen2(){
+   System.out.println("Bucket");
+}
+
+// Added by Naveen -11958
+
+}

1baf4be8a1bc6d425a1d25b4844914f1e41ab126
Sara <92007801+Sarabudheen@users.noreply.github.com>
2023-09-07 14:38:56 +0530

    Algorithm & Flowchart

---
+++ Sarabudheen-11957/Foundational Training/Algorithm & Flowchart/Day - 16/Algorithm_Flowchart_Ex01.java
@@ -0,0 +1,19 @@
+/***** Algorithm,Flowchart and Program for Addition of Two numbers... *****/
+package com.sara.day16;
+
+import java.util.Scanner;
+
+public class Algorithm_Flowchart_Ex01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First Number : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter the Second Number : ");
+		int num2 = sc.nextInt();
+		int sum = num1 + num2;
+		System.out.println("Addition of the Two Given Numbers is : " + sum);
+
+	}
+
+}

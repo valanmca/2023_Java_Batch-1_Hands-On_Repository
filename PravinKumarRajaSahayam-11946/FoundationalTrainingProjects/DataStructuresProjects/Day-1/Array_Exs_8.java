9f5d5aaf57205160d1c6c2a60e605f0df976fe0a
PravinKumarRajaSahaym <141833504+PravinKumarRajaSahaym@users.noreply.github.com>
2023-09-07 15:17:55 +0530

    Add files via upload

---
+++ PravinKumarRajaSahayam-11946/FoundationalTrainingProjects/DataStructuresProjects/Day-1/Array_Exs_8.java
@@ -0,0 +1,14 @@
+//Take the input from the user Syntax
+package com.pravinkumar.day1;
+import java.util.Scanner;
+public class Array_Exs_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Number:");
+    int Number=sc.nextInt();
+    System.out.println("Your Number is:" + Number);
+	}
+
+}

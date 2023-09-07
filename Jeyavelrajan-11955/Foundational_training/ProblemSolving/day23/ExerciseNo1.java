48a89127f0050471bcf6e4e75008e805bbfd2318
jeyavelRajan <73386152+jeyavelRajan@users.noreply.github.com>
2023-09-07 15:20:32 +0530

    problemSolving_Commit

---
+++ Jeyavelrajan-11955/Foundational_training/ProblemSolving/day23/ExerciseNo1.java
@@ -0,0 +1,23 @@
+package com.jeyavel.Algorithm.day16;
+import java.util.Scanner;
+
+public class ExerciseNo1 {
+
+	public static void main(String[] args) {
+		int num1,num2,sum;
+	System.out.println(" Enter The input values of num 1,num2");
+	@SuppressWarnings("resource")
+	Scanner sc = new Scanner (System.in);
+	 num1 =sc.nextInt();
+	 num2 = sc.nextInt();
+	 
+	 sum= num1+num2;
+	System.out.println("The sum of the two Values is:"+sum);
+		
+		
+
+	}
+
+
+
+}

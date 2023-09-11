a5b8be27cab3acc9516cc6f01bc13a4ad24d4974
Gayathri Kuppusamy <142476790+Gayathrik05@users.noreply.github.com>
2023-09-11 09:25:36 +0530

    Git/Bit Repository uploaded Successfully.

---
+++ Gayathri Devi-11954/Core Training/Git Repository/SeconddayGit/Collabgit/MyMainClass.java
@@ -0,0 +1,25 @@
+class Myclass{
+ void MyFile()
+ {
+  
+ }
+ void naveen(){
+  System.out.println("Hello All!");
+ }
+
+ void valan() {
+
+ }
+ void gayathri()
+ {
+  
+ }
+  public static MyMethod()
+  {
+    System.out.println("Sub");
+  }
+  public static void main(String []args){
+        MyMethod(); 
+  }
+}
+

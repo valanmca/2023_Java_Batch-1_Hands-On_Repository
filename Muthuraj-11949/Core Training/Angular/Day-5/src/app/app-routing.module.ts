c275dee7acecfd8909b6a2023b0ddc1709cf4ae5
MUTHURAJ.P <111838220+Muthu0706@users.noreply.github.com>
2023-09-24 14:30:27 +0530

    Add files via upload

---
+++ Muthuraj-11949/Core Training/Angular/Day-5/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path:'home',component:
+HomeComponent},
+{path:'Contact',component:
+ContactComponent},
+{path:'about',component:AboutComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

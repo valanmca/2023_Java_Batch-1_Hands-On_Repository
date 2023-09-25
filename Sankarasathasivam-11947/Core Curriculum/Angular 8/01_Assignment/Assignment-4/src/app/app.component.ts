43faaad76b27d212c98763ad2a45c51394eedbe3
Shiva-Rz <142473031+Shiva-Rz@users.noreply.github.com>
2023-09-25 11:06:14 +0530

    Assignment commit

---
+++ Sankarasathasivam-11947/Core Curriculum/Angular 8/01_Assignment/Assignment-4/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Forms';
+
+  myForm:FormGroup;
+  hide:boolean;
+  constructor(){
+      this.myForm = new FormGroup({
+        fName: new FormControl('',[Validators.required,Validators.pattern('[A-Za-z]+')]),
+        lName: new FormControl('',[Validators.required,Validators.pattern('[A-Za-z]+')]),
+        uName: new FormControl('',[Validators.required,Validators.pattern('[A-Za-z]+')]),
+        email: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+')]),
+        phone: new FormControl('',[Validators.required,Validators.pattern('[0-9]+'), Validators.maxLength(10),Validators.minLength(10)]),
+        password: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9!@#$%^&*]+'),Validators.minLength(6),Validators.maxLength(16)]),
+        cPwd: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9!@#$%^&*]+'),Validators.minLength(6),Validators.maxLength(16)]),
+      });
+      this.hide=true
+  }
+  register(data:any){
+    alert(data.name+""+data.email+""+data.age+""+data.phone)
+  }
+}

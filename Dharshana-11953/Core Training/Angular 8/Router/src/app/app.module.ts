a702da8697d39e6be14d1d5d0946d45069472261
Dharshana Alagarsamy <142382814+DharshuAlagar23@users.noreply.github.com>
2023-09-25 17:15:46 +0530

    Add files via upload

---
+++ Dharshana-11953/Core Training/Angular 8/Router/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES:Routes=[
+  {path:'home',component:HomeComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent}
+];
+
+@NgModule({
+  declarations: [
+   
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

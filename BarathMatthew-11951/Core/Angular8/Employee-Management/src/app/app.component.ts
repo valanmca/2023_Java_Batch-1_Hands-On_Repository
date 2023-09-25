a98add3293ddb35d7d9e37258431fa51305ce722
BarathMatthew <108358185+BarathMatthew@users.noreply.github.com>
2023-09-25 13:58:23 +0530

    Add files via upload

---
+++ BarathMatthew-11951/Core/Angular8/Employee-Management/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { Student } from './model/student';
+import { HospitalService } from './hospital.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'studentManage';
+  student:Student;
+  result:string="";
+  stuList:Student[]=[];
+  studentList:Student[]=[];
+  flag:boolean=false;
+  constructor(private service :HospitalService)
+  {
+    this.student=new Student();
+    this.getAllDetails();
+  }
+  changeDiv(){
+    this.flag=!this.flag;
+  }
+
+
+  insert(data:any)
+  {
+    this.student.id=data.id;
+    this.student.name=data.name;
+    this.student.dob=data.dob;
+    this.student.gender=data.gender;
+    this.student.depart=data.depart;
+    this.result=this.service.insert(this.student);
+    this.getAllDetails();
+
+
+  }
+  update(data:any)
+  {
+    this.student.id=data.id;
+    this.student.name=data.name;
+    this.student.dob=data.dob;
+    this.student.gender=data.gender;
+    this.student.depart=data.depart;
+    this.result=this.service.update(this.student);
+    this.getAllDetails();
+
+
+  }
+  delete(data:any)
+  {
+    this.student.id=data.id;
+    this.student.name=data.name;
+    this.student.dob=data.dob;
+    this.student.gender=data.gender;
+    this.student.depart=data.depart;
+    this.result=this.service.delete(this.student);
+    this.getAllDetails();
+
+
+  }
+
+
+  getAllDetails()
+  {
+    this.service.getAlldetails().subscribe(
+      students=>this.studentList=students
+    );
+
+
+  }
+
+
+  }
---
+++ BarathMatthew-11951/Core/Angular8/Employee-Management/src/app/hospital.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Student } from './model/student';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HospitalService {
+  private url:string="http://localhost:3004/posts";
+
+
+  constructor(private http:HttpClient) { }
+  insert(student:Student)
+  {
+    this.http.post(this.url,student).subscribe();
+    return "Record Inserted"
+  }
+  update(student:Student)
+  {
+    this.http.put(this.url+"/"+student.id,student).subscribe();
+    return "Record updated"
+  }
+
+
+  delete(student:Student)
+  {
+    this.http.delete(this.url+"/"+student.id).subscribe();
+    return "Record Deleted"
+  }
+  getAlldetails()
+  {
+    return this.http.get<Student[]>(this.url);
+  }
+
+
+  // getAllDetail(){
+  //   return this.http.get<Student[]>(this.url);
+
+
+  // }
+}

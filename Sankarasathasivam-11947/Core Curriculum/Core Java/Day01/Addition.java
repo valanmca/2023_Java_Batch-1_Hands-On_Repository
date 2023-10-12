408da4a43578a70b0b298a870df4ae4aaa0f097e
Shiva-Rz <142473031+Shiva-Rz@users.noreply.github.com>
2023-10-12 19:53:07 +0530

    Delete Sankarasathasivam-11947/Core Curriculum/Core Java/Day01 directory

--- Sankarasathasivam-11947/Core Curriculum/Core Java/Day01/Addition.java
+++
@@ -1,8 +0,0 @@
-public class Addition{
-   public static void main(String args[])
-   {
-   	int a=Integer.parseInt(args[0]);
-	int b=Integer.parseInt(args[1]);
-	System.out.println("Addition : "+(a+b));
-   }
-}

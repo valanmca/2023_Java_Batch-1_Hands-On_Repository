194aa4cd2453e1fbe32d8614b82fe59990e39259
Purushothaman C <91775559+Purushothaman1962@users.noreply.github.com>
2023-10-13 15:03:35 +0530

    Added Core Java Day-1

---
+++ Purushothaman-11959/Core/Core Java/Day-1/Factorial.java
@@ -0,0 +1,17 @@
+package com.purushoth.day_01;
+
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the number: ");
+     int num=sc.nextInt();
+     int fact=1;
+     for (int i=1;i<=num;i++) {
+    	 fact=fact*i;
+     }
+     System.out.println("Factorial of "+num+" is "+fact);
+	}
+
+}//5. Write a Java program to find the factorial of the given number.

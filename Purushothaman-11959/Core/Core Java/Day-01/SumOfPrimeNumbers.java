7bfa5db6854a1594099160308cde351447a78bdf
Purushothaman C <91775559+Purushothaman1962@users.noreply.github.com>
2023-10-24 15:40:13 +0530

    Committed Day-01 Java

---
+++ Purushothaman-11959/Core/Core Java/Day-01/SumOfPrimeNumbers.java
@@ -0,0 +1,31 @@
+package com.purushoth.day_01;
+
+public class SumOfPrimeNumbers {
+
+	public static void main(String[] args) {
+		 int n=0,i=1,j;
+		 int sum=0;
+		 
+		 while (n<10)
+		 {
+		 int count=0;
+		 for(j=1;j<=i;j++)
+		 {
+		 if(i%j==0)
+		 {
+		 
+		 count++;
+		 }
+		 }
+		 if(count==2)
+		 {
+		 sum=sum+i;
+		 n++;
+		 }
+		 i++;
+		 }
+		 
+		 System.out.println("Sum of first 10 prime numbers is "+sum);
+
+		 }
+}//1. Write a Java program to find the sum of the first 10 prime numbers.

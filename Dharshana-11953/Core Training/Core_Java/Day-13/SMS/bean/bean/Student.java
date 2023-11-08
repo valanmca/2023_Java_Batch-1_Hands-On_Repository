019b5346f48391968c557fd5bb86322c00d7279d
Dharshana Alagarsamy <142382814+DharshuAlagar23@users.noreply.github.com>
2023-11-08 10:29:51 +0530

    Add files via upload

---
+++ Dharshana-11953/Core Training/Core_Java/Day-13/SMS/bean/bean/Student.java
@@ -0,0 +1,49 @@
+package com.sms.bean;
+
+public class Student {
+	private int id;
+	private String name;
+	String dept;
+
+	public Student() {
+		super();
+	}
+
+	public Student(int id, String name, String dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public void display() {
+		System.out.println("Student Id : " + id);
+		System.out.println("Student Name : " + name);
+		System.out.println("Student Dept : " + dept);
+		System.out.println("-----------------------------------");
+	}
+}

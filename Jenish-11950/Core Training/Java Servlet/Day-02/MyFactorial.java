cf2e5b7097a6c44abc11b9620ef658680d15873d
Jenish Immanuel Raj J <144326843+Jenishrz@users.noreply.github.com>
2023-11-18 15:48:23 +0530

    *

---
+++ Jenish-11950/Core Training/Java Servlet/Day-02/MyFactorial.java
@@ -0,0 +1,41 @@
+package com.factorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFactorial
+ */
+@WebServlet("/MyFactorial")
+public class MyFactorial extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<boby>");
+		out.println("<center>");
+		out.println("<table border = 1 width = '60%'>");
+		out.println("<tr><th>Number</th><th>Factorial</th></tr>");
+		int fact = 1;
+        int val = 0;
+        for (int n = 1; n <=5; n++) {
+            fact = fact * n;
+            val++;
+
+        }
+        out.println("<tr><th>" + val + "</th><th>" + fact + "</th></tr>");
+        out.println("</table>");
+		out.println("</center>");
+		out.println("</boby>");
+		out.println("</html>");
+
+	}
+
+}

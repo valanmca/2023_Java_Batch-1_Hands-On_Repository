b83aaaf5e943244044d74bb7c7194b1f576e0691
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2023-11-30 15:47:32 +0530

    MainClass.java

--- Naveen - 11958/Core Training/Core Spring/01_Day/MainClass.java
+++ Naveen - 11958/Core Training/Core Spring/01_Day/MainClass.java
@@ -1,11 +1,8 @@
 package com.naveen;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+
 
 public class MainClass {
 
@@ -15,27 +12,18 @@ public class MainClass {
 		// emp.display();
 
 		// Reading data from the Ioc Container.
-		// ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
-		Resource resource = new ClassPathResource("Beans.xml");
-		BeanFactory factory = new XmlBeanFactory(resource); //Deprecated type
-
-		Employee bean = (Employee) factory.getBean("employee"); // Injection.
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		
+		Employee bean = (Employee) context.getBean("employee"); // Injection.
 		System.out.println(bean);
 
-		Employee bean1 = (Employee) factory.getBean("employee1");
+		Employee bean1 = (Employee) context.getBean("employee1");
 		System.out.println(bean1);
-		
-		
-//		Employee bean = (Employee) context.getBean("employee"); // Injection.
-//		System.out.println(bean);
-//
-//		Employee bean1 = (Employee) context.getBean("employee1");
-//		System.out.println(bean1);
 
 //		Address addr = (Address) context.getBean("address");
 //		System.out.println(addr);
-//
+
 //		Address addr1 = (Address) context.getBean("address1");
 //		System.out.println(addr1);
 

b6ee0527716d50ff3293075a5e9fc6fe9f5d6580
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2023-11-30 15:42:11 +0530

    Add files via upload

---
+++ Naveen - 11958/Core Training/Core Spring/01_Day/Address.java
@@ -0,0 +1,44 @@
+package com.naveen;
+
+public class Address {
+	private String city;
+	private String state;
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(String city, String state) {
+		super();
+		this.city = city;
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Address city=" + city + ", state=" + state + "";
+	}
+	
+
+//	public void display() {
+//		System.out.println("Employee City: " + city);
+//		System.out.println("Employee State: "+ state);
+//	}
+}

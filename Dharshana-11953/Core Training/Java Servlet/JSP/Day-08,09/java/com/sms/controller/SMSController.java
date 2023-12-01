6b4265912e577e305b1bf397e9e848b5e3a11642
Dharshana Alagarsamy <142382814+DharshuAlagar23@users.noreply.github.com>
2023-12-01 21:58:05 +0530

    #

---
+++ Dharshana-11953/Core Training/Java Servlet/JSP/Day-08,09/java/com/sms/controller/SMSController.java
@@ -0,0 +1,108 @@
+package com.sms.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sms.bean.StudentServ;
+import com.sms.dao.StudentDAO;
+
+
+
+
+@WebServlet("/SMSController")
+public class SMSController extends HttpServlet {
+
+		StudentDAO dao = new StudentDAO();
+		
+		protected void doPost(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			String requestFrom = request.getParameter("sms_button");
+			
+
+			if (requestFrom.equals("Insert Student")) {
+				int id = Integer.parseInt(request.getParameter("StuId"));
+				String name = request.getParameter("StuName");
+				String depart = request.getParameter("StuDepart");
+
+				StudentServ bean = new StudentServ(id, name, depart);
+				int n = dao.insertStudent(bean);
+
+				if (n == 1) {
+					response.sendRedirect("InsertStudentSuccess.jsp");
+				} else {
+					response.sendRedirect("InsertStudentFail.jsp");
+				}
+
+			}
+
+			if (requestFrom.equals("Update Student")) {
+				int id = Integer.parseInt(request.getParameter("StuId"));
+				String name = request.getParameter("StuName");
+				String depart = request.getParameter("StuDepart");
+
+				StudentServ bean = new StudentServ(id, name, depart);
+				int n = dao.updateStudent(bean);
+
+				if (n == 1) {
+					response.sendRedirect("UpdateStudentSuccess.jsp");
+				} else {
+					response.sendRedirect("UpdateStudentFail.jsp");
+				}
+
+			}
+			if (requestFrom.equals("Delete Student")) {
+				int id = Integer.parseInt(request.getParameter("StuId"));
+
+				int n = dao.deleteStudent(id);
+
+				if (n == 1) {
+					response.sendRedirect("DeleteStudentSuccess.jsp");
+				} else {
+					response.sendRedirect("DeleteStudentFail.jsp");
+				}
+
+			}
+
+			if (requestFrom.equals("Find Student")) {
+				int id = Integer.parseInt(request.getParameter("StuId"));
+
+				StudentServ bean = dao.findStudent(id);
+
+				RequestDispatcher rd = request.getRequestDispatcher("FindStudentSuccess.jsp");
+				request.setAttribute("bean", bean);
+				rd.forward(request, response);
+
+			}
+
+			
+		}
+		@Override
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+			String requestfrom =request.getParameter("delete");
+			
+			if(requestfrom==null) {
+	        List<StudentServ> list = dao.findAllStudent();
+	        request.setAttribute("list", list);
+	        RequestDispatcher rd = request.getRequestDispatcher("FindAll.jsp");
+	        rd.forward(request, response);
+			}
+			else {
+				
+		        List<StudentServ> list = dao.findAllStudent();
+		        request.setAttribute("msg", list);
+		        RequestDispatcher rd = request.getRequestDispatcher("DeleteStudent.jsp");
+		        rd.forward(request, response);
+				
+			}
+
+	}
+
+}
---
+++ Dharshana-11953/Core Training/Java Servlet/JSP/Day-08,09/java/com/sms/dao/StudentDAO.java
@@ -0,0 +1,105 @@
+package com.sms.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.sms.bean.StudentServ;
+import com.sms.util.DBUtil;
+
+
+
+public class StudentDAO {
+
+	public int insertStudent(StudentServ bean) {
+		int n = 0;
+		try {
+			Connection con = DBUtil.getDBConnection();
+			String sql = "insert into tbl_Student values(?,?,?)";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, bean.getId());
+			ps.setString(2, bean.getName());
+			ps.setString(3, bean.getDept());
+			n = ps.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return n;
+	}
+
+	public int deleteStudent(int id) {
+		int n = 0;
+		try {
+			Connection con = DBUtil.getDBConnection();
+			String sql = "delete from  tbl_Student where stud_id=?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			n = ps.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return n;
+	}
+
+	public int updateStudent(StudentServ stud2) {
+		int n = 0;
+		try {
+			Connection con = DBUtil.getDBConnection();
+			String sql = "update tbl_Student set stud_name=? , stud_depart=? where stud_id=?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(3, stud2.getId());
+			ps.setString(1, stud2.getName());
+			ps.setString(2, stud2.getDept());
+			n = ps.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return n;
+	}
+
+	public StudentServ findStudent(int id) {
+		StudentServ stud1 = null;
+		try {
+			Connection con = DBUtil.getDBConnection();
+			String sql = "select * from tbl_Student where stud_id=?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				stud1 = new StudentServ(id, sql, sql);
+				stud1.setId(rs.getInt("stud_id"));
+				stud1.setName(rs.getString("stud_name"));
+				stud1.setDept(rs.getString("stud_depart"));
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return stud1;
+	}
+	public List<StudentServ> findAllStudent() {
+		List<StudentServ> list = new ArrayList<StudentServ>();
+		try {
+			Connection con = DBUtil.getDBConnection();
+			String sql = "select * from tbl_Student";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				StudentServ bean = new StudentServ(0, sql, sql);
+				bean.setId(rs.getInt("stud_id"));
+				bean.setName(rs.getString("stud_name"));
+				bean.setDept(rs.getString("stud_depart"));
+				list.add(bean);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return list;
+	}
+}

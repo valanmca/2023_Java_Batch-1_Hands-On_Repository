a7e6e381e4d5988359036b05d929aaa576f86af0
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2023-12-07 15:36:55 +0530

    Add files via upload

---
+++ Naveen - 11958/Core Training/Hibernate/02_Day/java/com/naveen/Employee.java
@@ -0,0 +1,58 @@
+package com.naveen;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@NamedQueries({
+	@NamedQuery(name="SelectAllEmployee", query="from Employee")
+})
+
+
+@Entity
+@Table(name = "tbl_employee")
+public class Employee {
+	@Id
+	@Column(name = "emp_id")
+	private int id;
+
+	@Column(name = "emp_name")
+	private String name;
+	private int salary;
+
+	@Column(name = "salary")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
---
+++ Naveen - 11958/Core Training/Hibernate/02_Day/java/com/naveen/MainClass.java
@@ -0,0 +1,167 @@
+package com.naveen;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.NativeQuery;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml"); // connecting db and completing connection
+
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction tn = session.beginTransaction();
+
+//		Employee emp = new Employee();
+//		emp.setId(824);hql
+//		emp.setName("valan");5-Dec-2023	Day-69
+//		emp.setSalary(5000);
+//		
+//		session.save(emp); //Insert
+
+		// Select queries.
+
+		// String hql = "from Employee"; //select all
+		// String hql = "from Employee where id=3"; //select static
+		/*
+		 * String hql = "from Employee where id=:t"; //select dynamic Query<Employee>
+		 * query = session.createQuery(hql); query.setParameter("t", 104);
+		 * List<Employee> list = query.list();
+		 * 
+		 * for(Employee e : list) {
+		 * System.out.println(e.getId()+" "+e.getName()+" "+e.getSalary()); }
+		 * tn.commit(); session.close(); System.out.println("Object Saved");
+		 */
+
+		
+		
+		// Update Queries.
+		
+		
+//		String hql = "update Employee set salary=:s where id=:i";
+//		Query<Employee> query = session.createQuery(hql);
+//		query.setParameter("s", 3000);
+//		query.setParameter("i", 105);
+//		int n = query.executeUpdate();
+//		if(n==1)
+//			System.out.println("Record Updated!");
+//		else
+//			System.out.println("Record Not Updated!");
+//		
+//		tn.commit();
+//		session.close();
+//		System.out.println("Object Saved");
+
+		// Delete Query.
+
+//		String hql = "delete from Employee where id=:i";
+//		Query<Employee> query = session.createQuery(hql);
+//		query.setParameter("i", 149);
+//		int n = query.executeUpdate();
+//		if (n == 1)
+//			System.out.println("Record Deleted!");
+//		else
+//			System.out.println("Record Not Deleted!");
+//
+//		tn.commit();
+//		session.close();
+//		System.out.println("Object Saved");
+		
+		
+		//HCQL - select
+		
+//		Criteria query = session.createCriteria(Employee.class);
+//		//query.add(Restrictions.eq("id", 105));
+//		query.add(Restrictions.gt("salary", 3000));
+//		List<Employee> list  = query.list();
+//		for(Employee e : list) {
+//			System.out.println(e);
+//		}
+//		tn.commit();
+//		session.close();
+//		System.out.println("Object Saved");
+		
+		
+//		Criteria query = session.createCriteria(Employee.class);
+//		//query.add(Restrictions.eq("id", 105));
+//		//query.add(Restrictions.gt("salary", 3000));
+//		//query.add(Restrictions.like("name", "%K_%"));
+//		//query.addOrder(Order.asc("salary"));
+//		//query.addOrder(Order.desc("id"));
+//		query.add(Restrictions.between("salary", 2000, 5500));
+//		List<Employee> list  = query.list();
+//		
+//		
+//		if (list.isEmpty())
+//			System.out.println("No data present!");
+//		else {
+//			for(Employee e : list) {
+//				System.out.println(e);
+//			}
+//		}
+		
+		
+		
+		//Native query - select
+		
+//		NativeQuery<Employee> query =  session.createSQLQuery("select * from tbl_employee");
+//		query.addEntity(Employee.class);
+//		List<Employee> list = query.list();
+//		for(Employee e : list) {
+//		System.out.println(e);
+//		}
+//			
+//		tn.commit();
+//		session.close();
+//		System.out.println("Object Saved");
+		
+		
+		//Native query - delete
+//		NativeQuery<Employee> query =  session.createSQLQuery("delete from tbl_employee where emp_id=106");
+//		query.addEntity(Employee.class);
+//		int n = query.executeUpdate();
+//		if (n == 1)
+//			System.out.println("Record Deleted!");
+//		else
+//			System.out.println("Record Not Deleted!");
+//			
+//		tn.commit();
+//		session.close();
+//		System.out.println("Object Saved");
+		
+		//Native query - update
+//		NativeQuery<Employee> query =  session.createSQLQuery("update tbl_employee set salary = 1000 where emp_id=101");
+//		query.addEntity(Employee.class);
+//		int n = query.executeUpdate();
+//		if (n == 1)
+//			System.out.println("Record Updated!");
+//		else
+//			System.out.println("Record Not Updated!");
+//			
+//		tn.commit();
+//		session.close();
+//		System.out.println("Object Saved");
+		
+		
+		//Named Sql Query - select
+		
+		Query<Employee> query =  session.createNamedQuery("SelectAllEmployee");
+		List<Employee> list = query.list();
+		for(Employee e : list)
+			System.out.println(e);
+		
+		tn.commit();
+		session.close();
+	}
+
+}

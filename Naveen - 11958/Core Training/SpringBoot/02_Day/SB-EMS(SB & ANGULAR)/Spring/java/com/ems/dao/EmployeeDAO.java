ff1f93df889b497eee6f49d9099ae36962f7f0d0
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2023-12-14 15:52:14 +0530

    .

--- Naveen - 11958/Core Training/SpringBoot/02_Day/SB-EMS(SB & ANGULAR)/Spring/java/com/ems/dao/EmployeeDAO.java
+++ Naveen - 11958/Core Training/SpringBoot/02_Day/SB-EMS(SB & ANGULAR)/Spring/java/com/ems/dao/EmployeeDAO.java
@@ -1,23 +1,11 @@
 package com.ems.dao;
-//import java.util.List;
-//import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.ems.bean.Employee;
 													//bean type , primary key data type
 public interface EmployeeDAO extends CrudRepository<Employee, Integer>{
 	
-//	//These are the extra functionalities to add up on the controller.
-//	Employee findByName(String name);
-//	
-//	Employee findBySalary(int salary);
-//	
-//	void deleteByName(String ename);
-//	
-//	void deleteBySalary(int salary);
-//	
-//	//If we want to write our own query.
-//	@Query("SELECT * FROM tbl_employee where salary > 2000")
-//	List<Employee> getAllEmployeeByUsingSalary();
+
 }
 

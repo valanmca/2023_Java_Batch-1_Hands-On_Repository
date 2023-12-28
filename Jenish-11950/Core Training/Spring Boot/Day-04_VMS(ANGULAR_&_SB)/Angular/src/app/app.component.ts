1d2f9b6559d34d31853729567e81ea82fa696bdd
Jenish Immanuel Raj J <144326843+Jenishrz@users.noreply.github.com>
2023-12-28 16:05:47 +0530

    #

---
+++ Jenish-11950/Core Training/Spring Boot/Day-04_VMS(ANGULAR_&_SB)/Angular/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { Vehicle } from './model/vehicle';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'vehicle';
+  vehicle:Vehicle;
+  result:string="";
+  velList:Vehicle[]=[];
+  vehicleList:Vehicle[]=[];
+  flag:boolean=false;
+  constructor(private service :DataService)
+  {
+    this.vehicle=new Vehicle();
+    this.getAllDetails();
+  }
+  changeDiv(){
+    this.flag=!this.flag;
+  }
+
+
+  insert(data:any)
+  {
+    this.vehicle.id=data.id;
+    this.vehicle.name=data.name;
+    this.vehicle.type=data.vehicle;
+    this.vehicle.number=data.number;
+    this.vehicle.oname=data.oname;
+    this.result=this.service.insert(this.vehicle);
+    this.getAllDetails();
+
+
+  }
+  update(data:any)
+  {
+    this.vehicle.id=data.id;
+    this.vehicle.name=data.name;
+    this.vehicle.type=data.vehicle;
+    this.vehicle.number=data.number;
+    this.vehicle.oname=data.oname;
+    this.result=this.service.update(this.vehicle);
+    this.getAllDetails();
+
+
+  }
+  delete(data:any)
+  {
+    this.vehicle.id=data.id;
+    this.vehicle.name=data.name;
+    this.vehicle.type=data.vehicle;
+    this.vehicle.number=data.number;
+    this.vehicle.oname=data.oname;
+    this.result=this.service.delete(this.vehicle);
+    this.getAllDetails();
+
+
+  }
+
+  getAllDetails()
+  {
+    this.service.getAlldetails().subscribe(
+      vehicles=>this.vehicleList=vehicles
+    );
+
+
+  }
+}

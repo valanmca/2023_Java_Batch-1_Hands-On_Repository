0e992e5c315cb9f4295253c37b5fb7e0d01349e5
Gayathri Kuppusamy <142476790+Gayathrik05@users.noreply.github.com>
2023-12-28 15:46:26 +0530

    Add files via upload

---
+++ Gayathri Devi-11954/Core Training/Spring Boot/Frontend - Angular/Movie Management System/File 3/app/movie.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private _http:HttpClient) { }
+
+  
+  private url: string = 'http://localhost:1234';
+
+  addMovies(data:any):Observable<any>{
+    return this._http.post(this.url+"/PerformInsert",data);
+  }
+
+  getAllMovies():Observable<any>{
+    return this._http.get(this.url+"/ViewAll");
+  }
+
+  deleteMovies(id:number):Observable<any>{
+    return this._http.delete(this.url+"/PerformDelete/"+id);
+  }
+  updateMovie(id:number, data:any):Observable<any>{
+    return this._http.put(this.url+"/PerformUpdate",data);
+  }
+}

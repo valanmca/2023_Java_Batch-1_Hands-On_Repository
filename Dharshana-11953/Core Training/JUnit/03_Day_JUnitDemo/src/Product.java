13a9f9f55127f00a26d83d042ef103a25889f2b1
Dharshana Alagarsamy <142382814+DharshuAlagar23@users.noreply.github.com>
2023-12-29 16:06:01 +0530

    #

--- Dharshana-11953/Core Training/JUnit/03_Day_JUnitDemo/src/Product.java
+++
@@ -1,9 +0,0 @@
-package com.junit;
-
-public class Product {
-
-	public Product() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- Dharshana-11953/Core Training/JUnit/03_Day_JUnitDemo/src/ProductMaker.java
+++
@@ -1,9 +0,0 @@
-package com.junit;
-
-public class ProductMaker {
-
-	 public Product makeObject() {
-	        Product p = new Product();
-	        return p;
-	    }
-}

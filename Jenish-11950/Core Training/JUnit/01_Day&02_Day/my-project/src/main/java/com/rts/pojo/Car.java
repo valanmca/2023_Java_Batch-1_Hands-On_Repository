6c7632931dd0335c4b7e0c11124ba0f92fecf483
Jenish Immanuel Raj J <144326843+Jenishrz@users.noreply.github.com>
2024-01-04 15:12:10 +0530

    *

---
+++ Jenish-11950/Core Training/JUnit/01_Day&02_Day/my-project/src/main/java/com/rts/pojo/Car.java
@@ -0,0 +1,32 @@
+package com.rts.pojo;
+
+public class Car {
+	private String carNo;
+	private String manufactuer;
+	private String model;
+
+	public String getCarNo() {
+		return carNo;
+	}
+
+	public void setCarNo(String carNo) {
+		this.carNo = carNo;
+	}
+
+	public String getManufactuer() {
+		return manufactuer;
+	}
+
+	public void setManufactuer(String manufactuer) {
+		this.manufactuer = manufactuer;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+}

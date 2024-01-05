502567c9a490b9b3b072d781362df95f2f6cbe62
Domnicdev <142473218+Domnicdev@users.noreply.github.com>
2024-01-05 17:35:59 +0530

    DMS_SB

---
+++ Domnic_Dev -11964/Core Training/Spring Boot & Microservices/DMS_SpringBoot/SpringBoot/src/main/java/com/dms/bean/Doctor.java
@@ -0,0 +1,72 @@
+package com.dms.bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="table_doctor")
+public class Doctor {
+
+	@Id
+	@Column(name="d_id")
+	private int dId;
+	
+	@Column(name="d_name")
+	private String dName;
+	
+	@Column(name="d_department")
+	private String dDept;
+	
+	@Column(name="d_salary")
+	private int dSalary;
+
+	
+	
+	public Doctor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Doctor(int dId, String dName, String dDept, int dSalary) {
+		super();
+		this.dId = dId;
+		this.dName = dName;
+		this.dDept = dDept;
+		this.dSalary = dSalary;
+	}
+
+	public int getdId() {
+		return dId;
+	}
+
+	public void setdId(int dId) {
+		this.dId = dId;
+	}
+
+	public String getdName() {
+		return dName;
+	}
+
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+
+	public String getdDept() {
+		return dDept;
+	}
+
+	public void setdDept(String dDept) {
+		this.dDept = dDept;
+	}
+
+	public int getdSalary() {
+		return dSalary;
+	}
+
+	public void setdSalary(int dSalary) {
+		this.dSalary = dSalary;
+	}
+
+}

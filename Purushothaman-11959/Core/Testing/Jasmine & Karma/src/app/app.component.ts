fc8f44c1bab8cf4ab94a70262f8d6533b26c598b
Purushothaman <91775559+Purushothaman1962@users.noreply.github.com>
2024-01-08 17:57:49 +0530

    Jasmine & Karma Added.

---
+++ Purushothaman-11959/Core/Testing/Jasmine & Karma/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  addition(a : number, b : number) : number {
+    return a + b;
+  }
+  subtraction(a : number, b : number) : number {
+    return a - b;
+  }
+  multiplication(a : number, b : number) : number {
+    return a * b;
+  }
+  factorial(n : number) : number{
+    let fact = 1 ;
+    if (n<1){
+      fact = 0;
+    } else {
+    for(let i = 1;i <= n; i++) {
+      fact = fact * i;
+    }}
+  return fact;
+  }
+  isPrime(n : number) : boolean {
+    let temp:boolean = true;
+     if (n <=1 ) {
+       temp = false;
+     } for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+       temp = false;
+      }
+  }
+    return temp;
+  }
+}
